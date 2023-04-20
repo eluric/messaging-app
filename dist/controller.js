871cb4d7e984709db61378020d64248204b22b6c
Adam Zhang <azha3852@uni.sydney.edu.au>
2023-04-21 00:22:02 +1000

    using render instead of send

--- dist/controller.js
+++ dist/controller.js
@@ -4,33 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const fs_1 = require("fs");
-const templatePath = "./static/templates";
 const router = express_1.default.Router();
-const readHTMLFile = (filename) => {
-    let data = "";
-    (0, fs_1.readFile)(filename, (err, html) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(html.toString());
-        data = html.toString();
-        console.log(data);
-    });
-    return data;
-};
-const homePage = (request, response) => {
-    // const header = readHTMLFile(`${templatePath}/header.html`);
-    // const body = readHTMLFile(`${templatePath}/index.html`);
-    // const tail = readHTMLFile(`${templatePath}/tailer.html`);
-    const header = (0, fs_1.readFileSync)(`${templatePath}/header.html`).toString();
-    const body = (0, fs_1.readFileSync)(`${templatePath}/index.html`).toString();
-    const tail = (0, fs_1.readFileSync)(`${templatePath}/tailer.html`).toString();
-    response.setHeader("Content-Type", "text/html");
-    response.send(`${header}${body}`);
-    response.end(tail);
-};
-router.get('/', homePage);
-router.get('/home', homePage);
+router.get('/', (request, response) => {
+    response.render('home', { loggedIn: true });
+});
 exports.default = router;
--- src/controller.ts
+++ src/controller.ts
@@ -1,41 +1,10 @@
 import express, { Response, Request } from 'express';
-import fs, { readFile, readFileSync } from 'fs';
-
-const templatePath = "./static/templates"
 
 const router = express.Router();
 
-const readHTMLFile = (filename: string): string => {
-    let data = "";
-    
-    readFile(filename, (err, html) => {
-        if (err) {
-            console.error(err);
-            return;
-        } 
-        
-        console.log(html.toString());
-        data = html.toString();
-        console.log(data);
-    });
+router.get('/', (request: Request, response: Response) => {
 
-    return data;
-}
-
-const homePage = (request: Request, response: Response) => {
-    // const header = readHTMLFile(`${templatePath}/header.html`);
-    // const body = readHTMLFile(`${templatePath}/index.html`);
-    // const tail = readHTMLFile(`${templatePath}/tailer.html`);
-    const header = readFileSync(`${templatePath}/header.html`).toString();
-    const body = readFileSync(`${templatePath}/index.html`).toString();
-    const tail = readFileSync(`${templatePath}/tailer.html`).toString();
-
-    response.setHeader("Content-Type", "text/html");
-    response.send(`${header}${body}`);
-    response.end(tail);
-};
-
-router.get('/', homePage);
-router.get('/home', homePage);
+    response.render('home', { loggedIn: true });
+});
 
 export default router;
